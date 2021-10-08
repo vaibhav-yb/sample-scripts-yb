75a310484ea7f6de7fdf98a7803da8770606d65d
Vaibhav Kushwaha <vkushwaha@yugabyte.com>
2021-10-08 18:31:10 +0530

    edits

--- CDCLoadTesting/src/main/java/com/yugabyte/App.java
+++ CDCLoadTesting/src/main/java/com/yugabyte/App.java
@@ -27,9 +27,11 @@ public class App
       PreparedStatement delete = conn.prepareStatement("delete from test where a = ?");
       System.out.println("Table created, waiting for 10 seconds to proceed...");
       Thread.sleep(10000);
-      
+
 
       for (int cnt = 0; cnt < 1000; ++cnt) {
+        System.out.println("Starting row insert...");
+        Thread.sleep(3000);
         for (int i = 0; i < 100; ++i) {
           insert.setInt(1, i);
           insert.setInt(2, i+1);
@@ -38,17 +40,23 @@ public class App
             System.out.println(String.format("Error while inserting (%d, %d), exiting...", i, i + 1));
             System.exit(0);
           }
+          System.out.println("Inserted row with a = " + i + " and b = " + (i + 1));
           insert.clearParameters();
         }
 
+        System.out.println("Starting row update now...");
+        Thread.sleep(3000);
         for (int i = 0; i < 100; ++i) {
           int res = statement.executeUpdate("update test set b = b + 1 where a = " + i + ";");
           if (res != 1) {
             System.out.println(String.format("Error while updating key %d, exiting...", i));
             System.exit(0);
           }
+          System.out.println("Row after update, a = " + i + " b = " + (i + 2));
         }
 
+        System.out.println("Starting row delete...");
+        Thread.sleep(3000);
         for (int i = 0; i < 100; ++i) {
           statement.execute("begin;");
           delete.setInt(1, i);

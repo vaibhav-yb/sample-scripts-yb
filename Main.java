8f9e23cda28377d6567efc8ea975e4ead785502e
Vaibhav Kushwaha <vkushwaha@yugabyte.com>
2021-10-08 15:15:25 +0530

    blah

--- Main.java
+++ Main.java
@@ -8,20 +8,13 @@ import java.util.List;
 
 public class Main {
   public static void main(String[] args) {
-    Connection conn;
     try {
-      conn = DriverManager.getConnection(
+      Connection conn = DriverManager.getConnection(
       "jdbc:postgresql://127.0.0.1:5433/yugabyte",
       "yugabyte",
       "yugabyte");
       conn.setAutoCommit(true);
-    } catch (SQLException e) {
-      System.out.println("SQL Exception caught...");
-    }
 
-    try {
-      if (conn == null)
-        throw new Exception("Connection conn not initialized...");
       Statement statement = conn.createStatement();
       statement.execute("drop table if exists test;");
       statement.execute("create table test (a int primary key, b int);");

69bf81253ab7a147237a036a68b5729c52bf37c0
Vaibhav Kushwaha <vkushwaha@yugabyte.com>
2021-10-11 13:33:02 +0530

    changes

--- CDCLoadTesting/src/main/java/com/yugabyte/App.java
+++ CDCLoadTesting/src/main/java/com/yugabyte/App.java
@@ -72,14 +72,14 @@ public class App
         System.out.println("Starting row delete...");
         Thread.sleep(3000);
         for (int i = 0; i < internalOps; ++i) {
-          // statement.execute("begin;");
+          statement.execute("begin;");
           delete.setInt(1, i);
           int res = delete.executeUpdate();
           if (res != 1) {
             System.out.println(String.format("Error while deleting key %d, exiting...", i));
             System.exit(0);
           }
-          // statement.execute("commit;");
+          statement.execute("commit;");
           System.out.println("Deleted row with a = " + i);
           delete.clearParameters();
         }

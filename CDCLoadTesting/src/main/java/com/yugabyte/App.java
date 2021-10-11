930977e23ca314ca4e05b97eb86a929a1404fb93
Vaibhav Kushwaha <34186745+vaibhav-yb@users.noreply.github.com>
2021-10-11 14:01:53 +0530

    some changes

--- CDCLoadTesting/src/main/java/com/yugabyte/App.java
+++ CDCLoadTesting/src/main/java/com/yugabyte/App.java
@@ -30,7 +30,7 @@ public class App
       System.out.println("Table created, waiting for 10 seconds to proceed...");
       Thread.sleep(10000);
 
-      int numOfIterations = 5;
+      int numOfIterations = 4;
       int internalOps = 20;
       for (int cnt = 0; cnt < numOfIterations; ++cnt) {
         System.out.println("Starting row insert...");

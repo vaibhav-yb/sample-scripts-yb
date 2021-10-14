4d68fc2fcd70dd5180fcb0fee26252678e693f62
Vaibhav Kushwaha <34186745+vaibhav-yb@users.noreply.github.com>
2021-10-14 10:32:06 +0530

    added a text row in insert

--- CDCLoadTesting/src/main/java/com/yugabyte/App.java
+++ CDCLoadTesting/src/main/java/com/yugabyte/App.java
@@ -24,7 +24,7 @@ public class App
 
       // statement.execute("drop table if exists test;");
       // statement.execute("create table test (a int primary key, b int);");
-      PreparedStatement insert = conn.prepareStatement("insert into test values (?, ?)");
+      PreparedStatement insert = conn.prepareStatement("insert into test values (?, ?, ?)");
       PreparedStatement delete = conn.prepareStatement("delete from test where a = ?");
       PreparedStatement selectb = conn.prepareStatement("select b from test where a = ?");
       // System.out.println("Table created, waiting for 10 seconds to proceed...");
@@ -32,23 +32,25 @@ public class App
 
       int numOfIterations = 1000;
       int internalOps = 1000;
+      long totalOps = 0;
       for (int cnt = 0; cnt > -1; ++cnt) {
         System.out.println("Starting row insert...");
-        Thread.sleep(300);
+        // Thread.sleep(300);
         for (int i = 0; i < internalOps; ++i) {
           insert.setInt(1, i);
           insert.setInt(2, i+1);
+          insert.setString(3, "vaibhav")
           int res = insert.executeUpdate();
           if (res != 1) {
-            System.out.println(String.format("Error while inserting (%d, %d), exiting...", i, i + 1));
+            System.out.println(String.format("Error while inserting (%d, %d, %s), exiting...", i, i + 1, "vaibhav"));
             System.exit(0);
           }
-          System.out.println("Inserted row with a = " + i + " and b = " + (i + 1));
+          System.out.println("Inserted row with a = " + i + " and b = " + (i + 1) + " and c = vaibhav");
           insert.clearParameters();
         }
 
         System.out.println("Starting row update now...");
-        Thread.sleep(300);
+        // Thread.sleep(300);
         for (int i = 0; i < internalOps; ++i) {
           statement.execute("begin;");
           int res = statement.executeUpdate(String.format("update test set b = b + 1 where a = %d;", i));
@@ -65,12 +67,12 @@ public class App
             System.out.println(String.format("Error while updating key %d, exiting...", i));
             System.exit(0);
           }
-          System.out.println("Row after update, a = " + i + " b = " + (i + 2));
+          System.out.println("Row after update, a = " + i + " b = " + (i + 2) + " c = vaibhav");
           selectb.clearParameters();
         }
 
         System.out.println("Starting row delete...");
-        Thread.sleep(300);
+        // Thread.sleep(300);
         for (int i = 0; i < internalOps; ++i) {
           statement.execute("begin;");
           delete.setInt(1, i);
@@ -95,6 +97,8 @@ public class App
         }
 
         Thread.sleep(500);
+        totalOps += (3 * internalOps);
+        System.out.println("Total Insert+Update+Delete = " + totalOps);
       }
 
       // drop.close();
